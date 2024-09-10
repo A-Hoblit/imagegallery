e12e0ac9a4701b29d30a00c0a3c3ed271097edf9
Andrew Hoblit <70081309+A-Hoblit@users.noreply.github.com>
2024-09-10 14:11:37 -0700

    Database setup

--- README.md
+++ README.md
@@ -25,3 +25,21 @@ QOL NOTES:
   "scripts": {
     "dev": "next dev --turbo",
   }
+
+SCRIPTS:
+1. pnpm dev
+    opens a live preview locally
+2. pnpm run db:push 
+    applies schema changes to database
+3. pnpm run db:studio
+    opens a local site with a DB editor
+
+
+GIT COMMANDS:
+* All of these can be conveniently done in the github desktop app, what I usually use.
+1. git status
+    Lists modified files in repository
+2. git add -p
+    Goes through and reviews each change before adding them
+3. git commit -m "SUMMARY"
+    Commits all changes with listed summary description
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/d2d8aa81-8b7c-455e-bd21-04587f0d6e67-23ti.jpeg",
   "https://utfs.io/f/4f1b826e-51ed-4e93-a0fd-62811fc31cb6-e9gpms.jpg",
-  "https://utfs.io/f/02e676d8-f6a7-401d-86bb-2ad32af585bd-24vw.jpeg",
-  "https://utfs.io/f/4f1b826e-51ed-4e93-a0fd-62811fc31cb6-e9gpms.jpg",
-  "https://utfs.io/f/d2d8aa81-8b7c-455e-bd21-04587f0d6e67-23ti.jpeg",
-  "https://utfs.io/f/4f1b826e-51ed-4e93-a0fd-62811fc31cb6-e9gpms.jpg",
-  "https://utfs.io/f/d2d8aa81-8b7c-455e-bd21-04587f0d6e67-23ti.jpeg",
-  "https://utfs.io/f/02e676d8-f6a7-401d-86bb-2ad32af585bd-24vw.jpeg",
-  "https://utfs.io/f/4f1b826e-51ed-4e93-a0fd-62811fc31cb6-e9gpms.jpg",
-  "https://utfs.io/f/d2d8aa81-8b7c-455e-bd21-04587f0d6e67-23ti.jpeg",
-  "https://utfs.io/f/02e676d8-f6a7-401d-86bb-2ad32af585bd-24vw.jpeg",
-  "https://utfs.io/f/4f1b826e-51ed-4e93-a0fd-62811fc31cb6-e9gpms.jpg",
-  "https://utfs.io/f/d2d8aa81-8b7c-455e-bd21-04587f0d6e67-23ti.jpeg",
   "https://utfs.io/f/02e676d8-f6a7-401d-86bb-2ad32af585bd-24vw.jpeg",
 ];
 
@@ -22,10 +12,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
